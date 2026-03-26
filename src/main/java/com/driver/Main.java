84724ff79cb13b406379823a49367bfb43a4763a
Azam <ajamkhan02003@gmail.com>
2026-03-26 15:46:59 +0000

    now done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,13 +3,19 @@ package com.driver;
 public class Main {
 
     public static void main(String args[]){
+        // Task 2: create object of Product in Main function called p
         Product p = new Product();
 
+        // Task 3: call product(int, int)
         System.out.println(p.product(3, 4));
-        System.out.println(p.product(2, 3, 4));
-        System.out.print(p.product(3.00000, 9.00000));
 
+        // Task 4: call product(int, int, int)
+        System.out.println(p.product(2, 3, 4));
+
+        // Task 5: call product(double, double)
+        System.out.println(p.product(3.00000, 9.00000));
     }
+
     static class Product{
     public int product(int x, int y) {
         return x*y;
